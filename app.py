a55b3e06ddf5a96755523fc735a299944bb90c8f
Jahanzaib Malik <jahanzaib.malik@pakeventures.com>
2022-03-09 03:23:53 +0500

    Add Implementation for Support

--- app.py
+++ app.py
@@ -219,6 +219,24 @@ def affiliation():
     return render_template('affiliation.html', cssFile="dashboard", current_user=current_user, pageTitle="Affiliation")
 
 
+@app.route('/support', methods=['GET', 'POST'])
+def support():
+    redirectIfSessionNotExists()
+    current_user = User.query.filter_by(id=session['user_id']).first()
+    if request.method == 'POST':
+        current_user.strategy.currency_id = request.form['currency_id']
+        current_user.strategy.aggressiveness = request.form['aggressiveness']
+        current_user.strategy.stop_loss = request.form['stop_loss']
+        current_user.strategy.minimum_gains = request.form['minimum_gains']
+        current_user.strategy.audacity = request.form['audacity']
+        try:
+            db.session.commit()
+            flash("Strategy updated successfully", "success")
+        except:
+            flash("Error in updating Strategy", "danger")
+    return render_template('support.html', cssFile="dashboard", current_user=current_user, pageTitle="Support")
+
+
 @app.context_processor
 def context_processor():
     return dict(logoURL=url_for('static', filename='images/logo.png'))
